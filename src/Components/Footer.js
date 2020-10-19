5695dfae57298dba8431fa131a89546b88299ab6
Mark Leung <mark.leung89@gmail.com>
2020-10-20 10:45:16 +1100

    Latest portfolio changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -18,12 +18,9 @@ class Footer extends Component {
               {networks}
            </ul>
 
-           <ul>
-            <li>Modified with love by the Clever Programmer team ♥️</li>
-           </ul>
 
            <ul className="copyright">
-              <li>&copy; All credit goes to Tim Baker - Copyright 2017 Tim Baker</li>
+              <li>&copy; Copyright 2020 Mark Leung</li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
 
