a1dc8ccdb53f3951ea873e1c3138d0941d251dd0
Mark Leung <mark.leung89@gmail.com>
2020-10-20 11:53:11 +1100

    Latest chanes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -21,7 +21,6 @@ class Footer extends Component {
 
            <ul className="copyright">
               <li>&copy; Copyright 2020 Mark Leung</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
 
         </div>
